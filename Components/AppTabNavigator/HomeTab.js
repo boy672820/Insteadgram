d986a24fad793605e103f1cd99c0809152184147
이선주 <seonzoo@iseonjuui-MacBook-Pro.local>
2019-02-08 20:36:04 +0900

    Added to Story Header and get follower data

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Container, Content, Icon } from 'native-base';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Container, Content, Icon, Thumbnail } from 'native-base';
 import CardComponent from '../CardComponent';
 
 export default class HomeTab extends Component {
@@ -12,14 +12,23 @@ export default class HomeTab extends Component {
 	};
 
 	state = {
-		feeds: []
+		feeds: [],
+		followings: []
 	};
 
 	componentWillMount() {
+		// Get feeds
 		this.fetchFeeds().then( feeds => {
 			this.setState( {
 				feeds
 			} );
+		} );
+
+		// Get follower
+		this.fetchFollowing().then( followings => {
+			this.setState( {
+				followings
+			} )
 		} );
 	}
 
@@ -43,12 +52,65 @@ export default class HomeTab extends Component {
 		.then( res => res.result );
 	}
 
+	// Get follower
+	fetchFollowing() {
+		const data = {
+			id: 2,
+			jsonrpc: '2.0',
+			method: 'call',
+			params: [
+				'follow_api',
+				'get_following',
+				[ 'anpigon', '', 'blog', 10 ]
+			]
+		};
+
+		return fetch( 'https://api.steemit.com', {
+			method: 'POST',
+			body: JSON.stringify( data )
+		} )
+		.then( res => res.json() )
+		.then( res => res.result.map( ( { following } ) => following ) );
+	}
+
 	render() {
 		return (
 			<Container style={ style.container }>
 				<Content>
+					{/* Start Story header here */}
+					<View style={ { height: 100 } }>
+						<View style={ { flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 7 } }>
+							<Text style={ { fontWeight: 'bold' } }>Stories</Text>
+
+							<View style={ { flexDirection: 'row', 'alignItems': 'center' } }>
+								<Icon name="md-play" style={ { fontSize: 14 } }></Icon>
+								<Text style={ { fontWeight: 'bold' } }> Watch All</Text>
+							</View>
+						</View>
+
+						<View style={ { flex: 3 } }>
+							<ScrollView
+								horizontal={ true }
+								showsHorizontalScrollIndicator={ false }
+								contentContainerStyle={ {
+									alignItems: 'center',
+									paddingStart: 5,
+									paddingEnd: 5
+								} }>
+								{
+									this.state.followings.map( following =>
+										<Thumbnail
+											style={ { marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 } }
+											source={ { uri: `https://steemitimages.com/u/${following}/avatar` } } /> )
+								}
+							</ScrollView>
+						</View>
+					</View>
+					{/* End Story header */}
 					{
-						this.state.feeds.map( feed => <CardComponent data={ feed } /> )
+						this.state.feeds.map( feed => (
+							<CardComponent data={ feed } key={ feed.url } />
+						) )
 					}
 				</Content>
 			</Container>
